c81f9c550adf1703d4c0428798d0c31173baa618
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-07-08 00:33:19 +0200

    export to rethink

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,5 +1,6 @@
 (ns app.core
   (:require [cljs.nodejs :as node]
+            [cljs.core.match :refer-macros [match]]
             [cljs.core.async :refer [<! put! close! chan >!]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
@@ -10,6 +11,11 @@
 (def ^:private js-request (node/require "request"))
 
 (node/enable-util-print!)
+(def r (node/require "rethinkdb"))
+(def db-config {:host (.. js/process -env -RETHINK_HOST)
+                :port (.. js/process -env -RETHINK_PORT)
+                :ssl {:ca (.. js/process -env -RETHINK_CERT)}
+                :authKey (.. js/process -env -RETHINK_AUTH_KEY)})
 
 (defn- -request [url-or-opts]
   (let [c-resp (chan 1)]
@@ -42,6 +48,38 @@
       second
       :data))
 
+
+(defn event->payload [event]
+  (-> event
+      (js->clj :keywordize-keys true)
+      (extract-payload)
+      (convert-payload)
+      (dissoc :id)))
+
+(defn db-connect []
+  (let [c (chan)]
+    (.connect r (clj->js db-config)
+              #(go
+                 (if %1
+                   (>! c {:error %1})
+                   (>! c {:connection %2}))))
+    c))
+
+(defn handle-response [res]
+  (match [res]
+         [{:connection _}] (:connection res)
+         [{:error _}] (do
+                        (println (:error res))
+                        nil)))
+
+(defn insert-resource [conn resource]
+  (let [c (chan)
+        db    (.db r "offcourse")
+        table (.table db "resources")
+        opp  (.insert table (clj->js resource))]
+    (.run opp conn #(go (>! c (or %1 %2))))
+    c))
+
 #_(defn create-message [link]
   {:Data (.stringify js/JSON (clj->js link))
    :StreamName "expanded-links"
@@ -55,12 +93,17 @@
 (defn ^:export handler [event context cb]
   (go
     (let [event (js->clj event :keywordize-keys true)
+          connection (-> (<! (db-connect))
+                         handle-response )
           payload (-> event
                       (extract-payload)
                       (convert-payload))
-          resource (<! (request (:url payload)))]
-      (println (clj->js resource))
-      (cb nil (clj->js resource)))))
+          resource-js (->> (<! (request (:url payload)))
+                       (.parse js/JSON))
+          resource (js->clj resource-js :keywordize-keys true)
+          response (<! (insert-resource connection resource))]
+      (println resource-js)
+    (.close connection #(cb nil (clj->js response))))))
 
 (defn -main [] identity)
 (set! *main-cli-fn* -main)
